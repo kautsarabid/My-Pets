ddd65eba5f3c796333ff27ebf45b092ee8dba1a6
Kautsar Abid <abidkautsar@gmail.com>
2023-06-09 08:41:43 +0700

    menambahkan import custom pendataan hewan

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,4 @@ import './component/app-form-masuk-custom.js';
 import './component/app-footer-custom.js';
 import './component/app-form-daftar-custom.js';
 import './component/app-layanan-custom.js';
+import './component/app-form-pendataan-hewan-custom.js';
