08b0872af40ca7b5a9d4ceb76ae11b4db377babc
Kautsar Abid <abidkautsar@gmail.com>
2023-06-19 05:49:28 +0700

    menambahkan id schema user

--- src/scripts/model/data-hewan.js
+++ src/scripts/model/data-hewan.js
@@ -36,27 +36,12 @@ const dataHewanSchema = new mongoose.Schema({
       type: String,
     },
   }],
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 const DataHewan = mongoose.model('DataHewan', dataHewanSchema)
-
-
-
-// Menambah 1 data
-// const dataHewan1 = new DataHewan({
-//   nama: 'Ben',
-//   jenis: 'Kucing',
-//   umur: '3 Tahun',
-//   beratBadan: '23 Kg',
-//   vaksin: false,
-//   jadwalMakan: {
-//     hari: 'pagi',
-//     waktu: '04.00',
-//     tanggal: '06-06-2023',
-//   }
-// });
-
-// Simpan ke collection
-// dataHewan1.save().then((dataHewan) => console.log(dataHewan));
 
 module.exports = DataHewan;
