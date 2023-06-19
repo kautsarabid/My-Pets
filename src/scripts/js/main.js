92ff0c0e40682258afeb470a2e4b8fa3c550ae34
Kautsar Abid <abidkautsar@gmail.com>
2023-06-19 07:46:02 +0700

    menghapus file

--- src/scripts/js/main.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.getElementByName('jadwalMakan');
-
-checkbox.addEventListener('change', (event) => {
-  if (event.target.checked) {
-    checkbox.disabled = true;
-    // Lakukan logika penjadwalan makan
-    console.log('Makan telah dijadwalkan');
-  }
-});
