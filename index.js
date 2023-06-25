c724864686634620f260b8b204f9902647003022
Kautsar Abid <abidkautsar@gmail.com>
2023-06-26 05:55:07 +0700

    mengubah path static

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ app.use(methodOverride('_method'));
 
 // Built-in middleware
 app.use(expressLayout);
-app.use(
-  '/static',
-  express.static(path.join(__dirname, './src/public')),
-);
-// app.use(express.static(path.join(__dirname, './src/public')));
+// app.use(
+//   '/static',
+//   express.static(path.join(__dirname, './src/public')),
+// );
+app.use(express.static(path.join(__dirname, './src/public')));
 app.use(express.urlencoded({ extended: true }));
 
 // Menggunakan EJS
