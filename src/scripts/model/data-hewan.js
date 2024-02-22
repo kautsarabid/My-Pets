e5d6dbe56c23cba7ac40910cd10350fb8d485d1e
kautsarabid <abidkautsar@gmail.com>
2024-02-22 14:32:50 +0700

    menghapus nav pada button login

--- src/scripts/model/data-hewan.js
+++ src/scripts/model/data-hewan.js
@@ -1,47 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Membuat Schema
 const dataHewanSchema = new mongoose.Schema({
-  nama: {
-    type: String,
-  },
-  jenis: {
-    type: String,
-  },
-  umur: {
-    type: String,
-  },
-  beratBadan: {
-    type: String,
-  },
-  vaksin: {
-    type: String,
-  },
-  jadwalMakan: [{
-    hariMakan: {
-      type: String
-    },
-    waktuMakan: {
-      type: String
-    },
-    tanggalMakan: {
-      type: String
-    },
-  }],
-  tiketVaksin: [{
-    waktuVaksin: {
-      type: String,
-    },
-    tanggalVaksin: {
-      type: String,
-    },
-  }],
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
+	nama: {
+		type: String,
+	},
+	jenis: {
+		type: String,
+	},
+	umur: {
+		type: String,
+	},
+	beratBadan: {
+		type: String,
+	},
+	vaksin: {
+		type: String,
+	},
+	jadwalMakan: [
+		{
+			hariMakan: {
+				type: String,
+			},
+			waktuMakan: {
+				type: String,
+			},
+			tanggalMakan: {
+				type: String,
+			},
+		},
+	],
+	tiketVaksin: [
+		{
+			waktuVaksin: {
+				type: String,
+			},
+			tanggalVaksin: {
+				type: String,
+			},
+		},
+	],
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
 });
 
-const DataHewan = mongoose.model('DataHewan', dataHewanSchema)
+const DataHewan = mongoose.model("DataHewan", dataHewanSchema);
 
 module.exports = DataHewan;
